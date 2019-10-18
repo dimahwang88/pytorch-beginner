e9feedb7541983017827ed5fbcb25815cdbfa57f
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2019-10-18 20:48:35 +0200

    gcp

--- run_autoencoder.sh
+++ run_autoencoder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #gsutil cp gs://bepro-server-storage/dsort_tracking_data/"$rec_id"/hypotheses_1.txt dsort_out/ 
 source activate open-mmlab
-gsutil cp gs://bepro-server-storage/dsort_tracking_data/autoencoder_data/pwd_dataset.txt .
-python bepro_conv_autoencoder.py pwd_dataset.txt
+gsutil cp gs://bepro-server-storage/dsort_tracking_data/autoencoder_data/pwd_dataset.txt ./08-AutoEncoder/
+python ./08-AutoEncoder/bepro_conv_autoencoder.py ./08-AutoEncoder/pwd_dataset.txt
