ae897ba54c987b3ebe799585ce340ac78d89978b
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2019-10-25 10:37:23 +0200

    gcp

--- 08-AutoEncoder/bepro_conv_autoencoder.py
+++ 08-AutoEncoder/bepro_conv_autoencoder.py
@@ -46,26 +46,42 @@ class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, (5,5), stride=(2,2), padding=(2,2)),  # b, 64, 64, 64
+#            nn.Conv2d(3, 64, (5,5), stride=(2,2), padding=(2,2)),  # b, 64, 64, 64
+#            nn.ReLU(True),
+#            nn.Conv2d(64, 32, (3,3), stride=(2,2), padding=(1,1)),  # b, 32, 32, 32
+#            nn.ReLU(True),
+#            nn.Conv2d(32, 16, (3,3), stride=(2,2), padding=(1,1)),  # b, 16, 16, 16
+#            nn.ReLU(True),
+#            nn.Conv2d(16, 4, (3,3), stride=(2,2), padding=(1,1)),  # b, 4, 8, 8
+#            nn.ReLU(True)
+
+            nn.Conv2d(3, 64, (5,5), stride=(2,2), padding=(1,1)),  # b, 64, 64, 64
             nn.ReLU(True),
-#            nn.MaxPool2d(2, stride=(2,2)),  # b, 16, 32, 32
             nn.Conv2d(64, 32, (3,3), stride=(2,2), padding=(1,1)),  # b, 32, 32, 32
             nn.ReLU(True),
-#            nn.MaxPool2d(2, stride=(2,2)),  # b, 8, 8, 8
             nn.Conv2d(32, 16, (3,3), stride=(2,2), padding=(1,1)),  # b, 16, 16, 16
             nn.ReLU(True),
             nn.Conv2d(16, 4, (3,3), stride=(2,2), padding=(1,1)),  # b, 4, 8, 8
             nn.ReLU(True)
+
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(4, 16, (3,3), stride=(2,2), padding=(0,0)),  # b, 16, 17, 17
+#            nn.ConvTranspose2d(4, 16, (3,3), stride=(2,2), padding=(0,0)),  # b, 16, 17, 17
+#            nn.ReLU(True),
+#            nn.ConvTranspose2d(16, 32, (3,3), stride=(2,2), padding=(1,1)),  # b, 32, 33, 33
+#            nn.ReLU(True),
+#            nn.ConvTranspose2d(32, 64, (3,3), stride=(2,2), padding=(1,1)),  # b, 64, 65, 65
+#            nn.ReLU(True),
+#            nn.ConvTranspose2d(64, 3, (4,4), stride=(2,2), padding=(2,2)),  # b, 3, 128, 128
+#            nn.Tanh()
+            nn.ConvTranspose2d(4, 16, (3,3), stride=(2,2), padding=(1,1)),  # b, 16, 17, 17
             nn.ReLU(True),
             nn.ConvTranspose2d(16, 32, (3,3), stride=(2,2), padding=(1,1)),  # b, 32, 33, 33
             nn.ReLU(True),
             nn.ConvTranspose2d(32, 64, (3,3), stride=(2,2), padding=(1,1)),  # b, 64, 65, 65
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 3, (4,4), stride=(2,2), padding=(2,2)),  # b, 3, 128, 128
+            nn.ConvTranspose2d(64, 3, (5,5), stride=(2,2), padding=(1,1)),  # b, 3, 128, 128
             nn.Tanh()
         )
 
